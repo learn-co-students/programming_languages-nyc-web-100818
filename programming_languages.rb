bfab18ac8c47e5832bcffe8c91bf4637aef0d7d4
github username <github email address>
2018-09-22 10:14:59 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+
+  languages.each do |style, lang_hash|
+
+    lang_hash.each do |lang_name, type_hash|
+
+      type_hash.each do |key, value|
+        if new_hash.keys.include?(lang_name) # if key already exists...
+          new_hash[lang_name][:style] << style
+        else # otherwise add a new key 
+        new_hash[lang_name] = {
+          :type => value,
+          :style => [style]
+        }
+        end
+      end
+    end
+  end
+  new_hash
 end
+
+
+
+
+
+# def reformat_languages(languages)
+#   # your code here
+#   languages.each do |style, lang_hash|
+#       style = :oo
+#       lang_hash = {:ruby=>{:type=>"interpreted"},
+#                   :javascript=>{:type=>"interpreted"},
+#                   :python=>{:type=>"interpreted"},
+#                   :java=>{:type=>"compiled"}}
+
+#     lang_hash.each do |lang_name, type_hash|
+#         lang_name = :ruby
+#         type_hash = {:type=>"interpreted"}
+
+#       type_hash.each do |key, value|
+#         key = :type
+#         value = "interpreted"
+#       end
+#     end
+#   end
+#
+# end
