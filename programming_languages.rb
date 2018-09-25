b2a78478870e5fb99c68cd14eda572ae767eab8c
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 12:38:42 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, value| # type = oo / value = big block
+    value.each do |lang,v|# lang = ruby / v = {type=>"interpreted"}
+      style = {style: [type]}
+      new_hash[lang][:style] << type if new_hash[lang] != nil
+      new_hash[lang] = v.merge!(style) if new_hash[lang] == nil
+    end
+  end
+  puts new_hash
+  new_hash
 end
+
+
+=begin
+languages = {
+  :oo => {
+    :ruby => {:type => "interpreted"},
+    :javascript => {:type => "interpreted"},
+    :python => {:type => "interpreted"},
+    :java => {:type => "compiled"}
+  },
+  :functional => {
+    :clojure => {:type => "compiled"},
+    :erlang => {:type => "compiled"},
+    :scala => {:type => "compiled"},
+    :javascript => {:type => "interpreted"}
+  }
+}
+
+reformat_languages(languages)
+=end
